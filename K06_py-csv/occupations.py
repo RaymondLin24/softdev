1c58087b9fb53b164a99f6a97bef0c5035fee52c
Raymond Lin <rlin51@stuy.edu>
2024-09-20 11:34:26 -0400

    Finished K06 and added app.py to my workshop

--- K06_py-csv/occupations.py
+++ K06_py-csv/occupations.py
@@ -3,20 +3,30 @@
 #SoftDev
 #Reading a CSV File
 #2024/9/19
-#
+#1 hour
 
 import random
-import csv
+occupationsList = []
+with open("occupations.csv","r") as file:
+    f = file.read().split("\n")
+    for item in f:
+        item.split(',')
+        print(item + "\n")
+        print(occupationsList)
+        if (item[0] != "Total"):
+            occupationsList.append({item[0],item[1]})
+    
+def randomOccupation():
+    percentagesum = 0;
+    for job in occupationsList:
+        percentagesum += job[0]
+    if (percentagesum == 0):
+        return null
+    x = random.randint(0,percentagesum)
+    for job in occupationsList:
+        x -= job[0]
+        if x <= 0:
+            return job[1]
+    return null
 
-with open('occupations.csv', newline='') as csvfile:
-
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-
-    for row in spamreader:
-        
-def random_occupation():
-    precentage = 0
-    occupation = ""
-    for jobs in occupation:
-        ##
-    print ("occupation" + "percenatge")
+print(randomOccupation)

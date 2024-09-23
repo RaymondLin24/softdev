9e6c8d4902f5f190501ea1c15ac8a909d402d47d
Raymond Lin <rlin51@stuy.edu>
2024-09-23 11:30:22 -0400

    Renamed my directories and finished my app.py file

--- 08_Teardown/app.py
+++ 08_Teardown/app.py
@@ -1,16 +1,21 @@
-# your heading here
+#Raymond Lin
+#Louie-Lin 4 Da Win
+#SoftDev
+#K08_Teardown/Printing to HTML/Using flask to print to HTML
+#2024/9/20
+#30 Minutes
 
 '''
 DISCO:
 <note any discoveries you made here... no matter how small!>
 
 QCC:
-0. 
-1. 
-2. 
-3. 
-4. 
-5. 
+0. Java, NetLogo, and many other languages as this is usually the simplest way for variable assignment
+1. File System Root
+2. To the console of the site
+3. Prints the name of the app 
+4. It appears as HTML, we can see it on the site
+5. Java
  ...
 
 INVESTIGATIVE APPROACH:
@@ -25,7 +30,7 @@ from flask import Flask
 app = Flask(__name__)                    # Q0: Where have you seen similar syntax in other langs?
 
 @app.route("/")                          # Q1: What points of reference do you have for meaning of '/'?
-def hello_world():
+def hello_world():                  
     print(__name__)                      # Q2: Where will this print to? Q3: What will it print?
     return "No hablo queso!"             # Q4: Will this appear anywhere? How u know?
 

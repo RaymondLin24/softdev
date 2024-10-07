c46689461751a4d0da03ccf0c2b699aaf579f805
Raymond Lin <rlin51@stuy.edu>
2024-10-07 11:08:31 -0400

    Syncing code

--- 13_combine/app.py
+++ 13_combine/app.py
@@ -1,20 +1,33 @@
 from flask import Flask, render_template
+import random, csv
 app = Flask(__name__)
+
+def readfile(f):
+    d = {}
+    with open (f, 'r') as listfile:
+        reader = csv.reader(listfile)
+        next(reader)
+        for row in reader:
+            job = row[0]
+            if job == "Total":
+                continue
+            d[job] = [float(row[1]),row[2]]
+    return d        
+
+def sel(d):
+    newlist = []
+    for num in list(d.values()):
+        newlist.append(num[0])
+    return random.choices(list(d.keys()), weights=newlist, k=1)
 
 @app.route("/")
 def hello_world():
     return "No hablo queso!"
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Q1: Can all of your teammates confidently predict the URL to use to load this page?
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-@app.route("/template") 
+@app.route("/wdywtbwygp")
 def test_tmplt():
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # Q2: What is the significance of each argument? Simplest, most concise answer best.
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    return render_template( 'tablified.html', collection=coll)
-
+    occupations = readfile("data/occupations.csv")
+    return render_template('tablified.html', collection1=occupations, occupation=sel(occupations))
 
 if __name__ == "__main__":
     app.debug = True

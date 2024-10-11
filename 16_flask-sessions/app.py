27fa015831e20ff220f546162a8aab207be63399
Raymond Lin <rlin51@stuy.edu>
2024-10-11 11:29:39 -0400

    Response page works now

--- 16_flask-sessions/app.py
+++ 16_flask-sessions/app.py
@@ -1,6 +1,8 @@
-# Clyde 'Thluffy' Sinclair
+# Kevin Lin, Raymond Lin, Christopher Louie
+# Death Row Coders
 # SoftDev
-# October 2024
+# October 11, 2024
+# 1 hour
 
 import os
 from flask import Flask
@@ -15,21 +17,21 @@ app.secret_key = secret
 
 @app.route("/")
 def disp_loginpage():
-    print(session.get('username'))
     if 'username' in session:
-        return "you're session exists"
+        return redirect("/response.html")
     return render_template( 'login.html' )
 
 @app.route("/response.html" , methods=['GET','POST'])
 def authenticate():
-    session['username'] = request.args.get('username')
-    return render_template( 'response.html', username = request.args['username'])
+    if(request.args.get('username') != None):
+        session['username'] = request.args.get('username')
+    return render_template( 'response.html', username = session['username'])
 
 @app.route("/logout")
 def logout():
     session.pop('username', None)
     return render_template('logout.html')
-    
+
 if __name__ == "__main__":
-    app.debug = True 
+    app.debug = True
     app.run()

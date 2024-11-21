413ce073d2f48d6fcb83333ec0fb84f9e089e481
Raymond Lin <rlin51@stuy.edu>
2024-11-21 11:13:34 -0500

    23

--- 23_rest/app.py
+++ 23_rest/app.py
@@ -1,16 +1,30 @@
-import requests
+import json
+import urllib.request
+from flask import Flask, render_template
 
-url = "https://api.nasa.gov/planetary/apod"
-f = open("key_nasa.txt", "r")
-api_key = f.read()
+app = Flask(__name__)
+with open('key_nasa.txt', 'r') as file:
+    API_KEY = file.read()
+APOD_URL = f'https://api.nasa.gov/planetary/apod?api_key={API_KEY}'
 
-params = {
-    "api_key": api_key
-}
+@app.route('/')
+def index():
+    # Fetches data from NASA API using urllib
+    with urllib.request.urlopen(APOD_URL) as response:
+        if response.status == 200:
+            # Read and decode the JSON response
+            data = json.loads(response.read().decode())
 
-response = requests.get(url, params=params)
+            # Extracts data from the api
+            title = data['title']
+            date = data['date']
+            explanation = data['explanation']
+            image_url = data['url']
 
-data = response.json()
-print("Title:", data['title'])
-print("Explanation:", data['explanation'])
-print("Image URL:", data['url'])
+            # Passes data to the HTML template
+            return render_template('main.html', title=title, date=date, explanation=explanation, image_url=image_url)
+        else:
+            return f"Error fetching data: {response.status}"
+
+if __name__ == '__main__':
+    app.run(debug=True)

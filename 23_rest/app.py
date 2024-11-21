dced4492a910662940ea3da5992e40a047890f10
Raymond Lin <rlin51@stuy.edu>
2024-11-21 11:17:57 -0500

    23

--- 23_rest/app.py
+++ 23_rest/app.py
@@ -9,19 +9,16 @@ APOD_URL = f'https://api.nasa.gov/planetary/apod?api_key={API_KEY}'
 
 @app.route('/')
 def index():
-    # Fetches data from NASA API using urllib
+
     with urllib.request.urlopen(APOD_URL) as response:
         if response.status == 200:
-            # Read and decode the JSON response
             data = json.loads(response.read().decode())
 
-            # Extracts data from the api
             title = data['title']
             date = data['date']
             explanation = data['explanation']
             image_url = data['url']
 
-            # Passes data to the HTML template
             return render_template('main.html', title=title, date=date, explanation=explanation, image_url=image_url)
         else:
             return f"Error fetching data: {response.status}"
